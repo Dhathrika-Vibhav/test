e5b66158013346f4328cbcc69b0682cab1fbddb3
unknown <Vibhavdhathrika@gmail.com>
2026-02-20 18:18:23 +0530

    Initial commit: Project setup

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcept";
+import TabButton from "./components/Tabbutton";
+import { EXAMPLES } from "./data";
+
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+
+
+    function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts"> 
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {!selectedTopic ? <p>Please select a topic</p>: null}
+            {selectedTopic && (
+              <>
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+              </div>
+              </>
+            )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
